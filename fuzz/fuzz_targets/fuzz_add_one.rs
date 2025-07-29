0701877b7c775becaff945c9cd0297c97007683a
Aaron Friel <mayreply@aaronfriel.com>
2025-07-29 19:35:29 +0900

    Fix fuzz target import

--- fuzz/fuzz_targets/fuzz_add_one.rs
+++ fuzz/fuzz_targets/fuzz_add_one.rs
@@ -1,8 +1,8 @@
 #![no_main]
 use flameview::add_one;
-use libafl_libfuzzer::fuzz;
+use libafl_libfuzzer::fuzz_target;
 
-fuzz!(|data: &[u8]| {
+fuzz_target!(|data: &[u8]| {
     if data.len() == 4 {
         let v = i32::from_le_bytes(data.try_into().unwrap());
         let _ = add_one(v);
